b7e8131466d3e387099eace4ecc0bef139bf70d0
Hyune-c <choi8608@gmail.com>
2022-12-10 18:26:30 +0900

    ♻️ v2 - 코드 개선

--- src/main/java/com/hyunec/samplecode/Batting.java
+++ src/main/java/com/hyunec/samplecode/Batting.java
@@ -1,23 +1,7 @@
 package com.hyunec.samplecode;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
-
-@Getter
-@RequiredArgsConstructor
 public enum Batting {
-    STRIKE(0),
-    BALL(1),
-    HIT(2);
-
-    private final Integer value;
-
-    public static Batting getByValue(final Integer value) {
-        return Arrays.stream(Batting.values())
-                .filter(i -> i.value.equals(value))
-                .findFirst()
-                .orElseThrow();
-    }
+    STRIKE,
+    BALL,
+    HIT;
 }
--- src/main/java/com/hyunec/samplecode/BattingRandomGenerate.java
+++ src/main/java/com/hyunec/samplecode/BattingRandomGenerate.java
@@ -7,12 +7,11 @@ import java.util.Random;
 @Component
 public class BattingRandomGenerate implements RandomGenerate {
 
-    private static final int BATTING_CASE_COUNT = 3;
-
     private static final Random RANDOM = new Random();
 
     @Override
     public Batting generate() {
-        return Batting.getByValue(RANDOM.nextInt(BATTING_CASE_COUNT));
+        Batting[] battings = Batting.values();
+        return battings[RANDOM.nextInt(battings.length)];
     }
 }
